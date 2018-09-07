5b6ad3a3799f51441775688a0af529deaee51c3a
alexiastandish <alexiastandish@gmail.com>
2018-09-07 16:21:35 -0500

    complete react app 5

--- app-5/src/App.js
+++ app-5/src/App.js
@@ -1,21 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import Image from './components/Image'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App 5</h1>
+        <Image />
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
---
+++ app-5/src/components/Image.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+
+function Image() {
+  return (
+    <div>
+      <img src="https://i.kym-cdn.com/photos/images/newsfeed/001/393/656/da7.jpg" />
+    </div>
+  )
+}
+export default Image
