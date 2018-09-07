4bcc70d8b3ef65322db8283a25e8fbb492ea3cb8
alexiastandish <alexiastandish@gmail.com>
2018-09-07 14:12:17 -0500

    complete react app 1

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -1,21 +1,30 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+  constructor() {
+    super()
+
+    this.state = {
+      userInput: '',
+    }
+
+    this.handleChange = this.handleChange.bind(this)
+  }
+
+  handleChange(val) {
+    this.setState({ userInput: val })
+  }
+
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>App 1</h1>
+        <input type="text" onChange={event => this.handleChange(event.target.value)} />
+        <p>{this.state.userInput}</p>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
